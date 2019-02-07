8c32d1f82adfafca1e96fe69dcedf7bc1a55b884
Gianluca Lagnese <glagnese@sissa.it>
2019-02-07 17:07:02 +0100

    changes copied from laptop

--- itebd/staggered/staggeredxxz.py
+++ itebd/staggered/staggeredxxz.py
@@ -7,18 +7,20 @@ from scipy.fftpack import fft
 def main(argv):
     cristo=mps(bond_dimension=300)
     hilbertsize=2**2
-    gpar=2
+    epsilon=0.001
+    gpar=-epsilon**(-1)
     h=5
 
-    HamA = np.diag([ +gpar*SzSz(conf,0,1)+h*Sz(conf,0)-h*Sz(conf,1)   for conf in range(hilbertsize)])
-    HamB = np.diag([ +gpar*SzSz(conf,0,1)-h*Sz(conf,0)+h*Sz(conf,1)   for conf in range(hilbertsize)])
+    HamA = np.diag([ -0.5*gpar*(4.*SzSz(conf,0,1)+1)-4.*h*Sz(conf,0)+4.*h*Sz(conf,1)   for conf in range(hilbertsize)])
+    HamB = np.diag([ -0.5*gpar*(4.*SzSz(conf,0,1)+1)+4.*h*Sz(conf,0)-4.*h*Sz(conf,1)   for conf in range(hilbertsize)])
     # off-diagonal part
     for conf in range(hilbertsize):
             value, newconf = Spinflip(conf,0,1)
-            HamA[newconf,conf] +=value     
-            HamB[newconf,conf] +=value     
-
-    delta=0.0005;T=20;L=int(T/delta)
+            HamA[newconf,conf] -=2.*value     
+            HamB[newconf,conf] -=2.*value     
+#    HamA=epsilon*HamA
+#    HamB=epsilon*HamB
+    delta=0.0001;T=100;L=int(T/delta)
     UA=np.reshape(expm(-complex(0,delta)*HamA),(2,2,2,2))
     UB=np.reshape(expm(-complex(0,delta)*HamB),(2,2,2,2))
 #    print(U)
@@ -33,12 +35,15 @@ def main(argv):
         corr[2][step]=4.*cristo.expectation_SzSz(3,connected=False)  
         cristo.evol(UA,UB)
 #    np.savetxt("cristo.out",corr)
-    corrf= fft(corr[2])
-    
-#    plt.plot(corr[0])
-#    plt.plot(corr[1])
-#    plt.plot(corr[2])
-    plt.plot(np.array([2*np.pi*i/L for i in range(1,L)]),1/L*np.abs(corrf[1:L]))
+#    corrf= fft(corr[2])
+    #time=np.arange(0,T,delta)
+    #plt.plot(time,corr[0])
+    #plt.plot(time,corr[1])
+    #plt.plot(time,corr[2])
+    plt.plot(np.array([2*np.pi*i/T for i in range(1,L//2)]),np.abs(fft(corr[0])[1:L//2]))
+    plt.plot(np.array([2*np.pi*i/T for i in range(1,L//2)]),np.abs(fft(corr[1])[1:L//2]))
+    plt.plot(np.array([2*np.pi*i/T for i in range(1,L//2)]),np.abs(fft(corr[2])[1:L//2]))
+    plt.yscale("log")
     plt.show()
 if __name__=="__main__":
     main(sys.argv[1:])
